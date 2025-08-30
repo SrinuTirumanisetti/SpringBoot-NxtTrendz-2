a39bd37d5c7ba5378e048f02817d3ce5de4fd58b
SrinuTirumanisetti <srinutirumanisetti@gmail.com>
2025-08-30 16:39:38 +0000

    Implemented category model

--- src/main/java/com/example/nxttrendz2/model/Category.java
+++ src/main/java/com/example/nxttrendz2/model/Category.java
@@ -1,9 +1,40 @@
-/*
- *
- * You can use the following import statements
- * 
- * import javax.persistence.*;
- * 
- */
+package com.example.nxttrendz2.model;
 
-// Write your code here
+import javax.persistence.*;
+
+@Entity
+@Table(name = "categories")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+
+    private String description;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
