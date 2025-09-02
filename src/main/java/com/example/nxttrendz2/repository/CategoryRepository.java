ba306020d4e84c240f9ac61af3de32b6e413645d
SrinuTirumanisetti <srinutirumanisetti@gmail.com>
2025-09-02 17:06:08 +0000

    Implemented CategoryRepository

--- src/main/java/com/example/nxttrendz2/repository/CategoryRepository.java
+++ src/main/java/com/example/nxttrendz2/repository/CategoryRepository.java
@@ -6,4 +6,15 @@
  * 
  */
 
-// Write your code here
+package com.example.nxttrendz2.repository;
+
+import com.example.nxttrendz2.model.Category;
+import java.util.ArrayList;
+
+public interface CategoryRepository {
+    ArrayList<Category> getAllCategories();
+    Category createCategory(Category category);
+    Category getCategoryById(int categoryId);
+    Category updateCategory(int categoryId, Category category);
+    void deleteCategory(int categoryId);
+}
