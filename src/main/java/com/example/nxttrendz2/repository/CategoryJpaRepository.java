fd153aff0827402a044e46e48b358dedaacce557
SrinuTirumanisetti <srinutirumanisetti@gmail.com>
2025-09-02 17:09:30 +0000

    Implemented CategoryJpaRepository

--- src/main/java/com/example/nxttrendz2/repository/CategoryJpaRepository.java
+++ src/main/java/com/example/nxttrendz2/repository/CategoryJpaRepository.java
@@ -7,4 +7,12 @@
  * 
  */
 
-// Write your code here
+package com.example.nxttrendz2.repository;
+
+import com.example.nxttrendz2.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryJpaRepository extends JpaRepository<Category, Integer> {
+}
